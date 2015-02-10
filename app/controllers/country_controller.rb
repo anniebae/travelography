8d85d01fed959fe9f6e2f51e1c1aefe9e020511d
anniebae <bae.annie@gmail.com>
2015-02-10 00:01:26 -0500

    html files created for all 7 countries

--- app/controllers/country_controller.rb
+++ app/controllers/country_controller.rb
@@ -1,4 +1,22 @@
 class CountryController < ApplicationController
   def wales
   end
+
+  def england
+  end
+
+  def france
+  end
+
+  def spain
+  end
+
+  def italy
+  end
+
+  def scotland
+  end
+
+  def the_netherlands
+  end
 end
