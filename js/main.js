f28f39c2ba6a5c1842a4cc46595f0401987e69ab
anniebae <bae.annie@gmail.com>
2015-02-12 13:20:14 -0500

    own photos added to bxslider

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-
-
-icons = [
-  {platform: 'blogger', img: 'stylesheets/images/social_media_icons/blogger.png'},
-  {platform: 'facebook', img: 'stylesheets/images/social_media_icons/facebook.png'},
-  {platform: 'instagram', img: 'stylesheets/images/social_media_icons/instagram.png'},
-  {platform: 'linkedin', img: 'stylesheets/images/social_media_icons/linkedin.png'},
-  {platform: 'tumblr', img: 'stylesheets/images/social_media_icons/tumblr.png'},
-  {platform: 'twitter', img: 'stylesheets/images/social_media_icons/twitter.png'}
-];
-facebook = {platform: 'facebook', img: 'stylesheets/images/social_media_icons/facebook.png'}
 $(document).ready(function () {
   $('.dropdown-toggle').dropdown();
   $('.bxslider').bxSlider({
@@ -22,10 +11,3 @@ var navbarView = new NavbarView();
 var bxsliderView = new BxsliderView();
 var socialMediaView = new SocialMediaView();
 var siteInfoView = new SiteInfoView();
-
-
-socialMediaIconsCollection = new SocialMediaIconsCollection(icons);
-
-$(function() {
-  console.log(socialMediaIconsCollection);
-});
