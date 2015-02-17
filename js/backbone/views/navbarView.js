1f6f88b8e02740734664fd11a3f4d4a92a7ee91c
anniebae <bae.annie@gmail.com>
2015-02-17 16:12:36 -0500

    england, about, and home rendered in backbone

--- js/backbone/views/navbarView.js
+++ js/backbone/views/navbarView.js
@@ -4,27 +4,36 @@ var NavbarView = Backbone.View.extend({
   homeBannerTemplate: _.template($("#home-banner-template").html()),
   aboutBannerTemplate: _.template($("#about-banner-template").html()),
   aboutBodyTemplate: _.template($("#about-body-template").html()),
+  englandTemplate: _.template($("#england-template").html()),
+  englandBannerTemplate: _.template($("#england-banner-template").html()),
+  franceTemplate: _.template($("#france-template").html()),
+  netherlandsTemplate: _.template($("#netherlands-template").html()),
+  scotlandTemplate: _.template($("#scotland-template").html()),
+  spainTemplate: _.template($("#spain-template").html()),
+  walesTemplate: _.template($("#wales-template").html()),
   initialize: function() {
-    $banner = $('#banner');
-    $body   = $('#body');
     this.renderNavbar();
   },
   events: {
     'click #about-btn': 'showAbout',
     'click #home-btn': 'showHome',
-    'click #netherlands-tab': 'showNetherlands'
+    'click #england-tab': 'showEngland'
   },
   renderNavbar: function() {
     this.$el.html(this.navbarTemplate());
     return this;
   },
   showAbout: function() {
-    $banner.html(this.aboutBannerTemplate());
-    $body.html(this.aboutBodyTemplate());
+    $('#banner').html(this.aboutBannerTemplate());
+    $('#body').html(this.aboutBodyTemplate());
     return this;
   },
   showHome: function() {
-    $banner.html(this.homeBannerTemplate());
+    $('#banner').html(this.homeBannerTemplate());
     return this;
+  },
+  showEngland: function() {
+    $('#banner').html(this.englandBannerTemplate());
+    $('#body').html(this.englandTemplate());
   },
 });
